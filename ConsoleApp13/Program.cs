a51508c7a8f18a9551163d85df4f764cb9e24490
Александр Александр <stripalol@inbox.ru>
2024-02-13 16:50:47 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            dateTime.print();
+        }
+    }
+    class LevelManager
+    {
+        private int _n;
+
+        public void NEWLevel(int n)
+        {
+            Console.WriteLine($"вы перешли на {n} уровень");
+        }
+
+    }
+
+    public static class MyExtension
+    {
+        public static int Sum(this int a,int b)
+        {
+            int c = a + b;
+            Console.WriteLine(c); 
+
+            return c;
+        }
+        public static void print(this DateTime n)
+        {
+            
+            Console.WriteLine(n);
+        }
+    }
+    public class Telephone
+    {
+        private readonly string brand;
+        private readonly string model;
+        private readonly int price;
+        private const string socket="Litnig";
+    }
+
+}
